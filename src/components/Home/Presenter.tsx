095e328ba87fe804775e09bf0a4adbe2e8057041
Hiroto-Shida <uecshida6295@gmail.com>
2024-10-10 09:02:22 +0900

    モアイ語から日本語への変換機能の実装

--- src/components/Home/Presenter.tsx
+++ src/components/Home/Presenter.tsx
@@ -1,26 +1,97 @@
-import { SubmitHandler, useFormContext } from "react-hook-form";
+import {
+  SubmitErrorHandler,
+  SubmitHandler,
+  useFormContext,
+} from "react-hook-form";
 import { FormType } from ".";
 import styles from "./index.module.scss";
 import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
 import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
+import { useEffect, useMemo, useRef } from "react";
+import clsx from "clsx";
+import React from "react";
 
 type Props = {
   translateToMoai: SubmitHandler<FormType>;
   translateToJp: SubmitHandler<FormType>;
+  translateInvalidToJp: SubmitErrorHandler<FormType>;
+  moaiLangText: string;
+  notMoaiLangIndices: number[];
+  handleChangeMoaiLang: (value: string) => void;
 };
 
-const Presenter: React.FC<Props> = ({ translateToMoai, translateToJp }) => {
-  const { register, handleSubmit } = useFormContext<FormType>();
+const Presenter: React.FC<Props> = ({
+  translateToMoai,
+  translateToJp,
+  translateInvalidToJp,
+  moaiLangText,
+  notMoaiLangIndices,
+  handleChangeMoaiLang,
+}) => {
+  const { register, handleSubmit, formState, setValue } =
+    useFormContext<FormType>();
+
+  const textMoaiRef = useRef<HTMLTextAreaElement | null>(null);
+  const displayAreaRef = useRef<HTMLDivElement | null>(null);
+
+  // モアイ語のテキストをスタイル付きで表示する
+  const styledMoaiLangText = useMemo(() => {
+    return moaiLangText.split("").map((char, index) => {
+      if (char === "\n") {
+        // MEMO: 改行した時の自動スクロール考慮
+        if (index === moaiLangText.length - 1)
+          return (
+            <React.Fragment key={index}>
+              <br />
+              <br />
+            </React.Fragment>
+          );
+        return <br key={index} />;
+      }
+      // TODO: spanを最小限にする
+      if (notMoaiLangIndices.includes(index)) {
+        return (
+          <span key={index} className={styles.error}>
+            {char}
+          </span>
+        );
+      }
+      return <span key={index}>{char}</span>;
+    });
+  }, [moaiLangText, notMoaiLangIndices]);
+
+  // 入力要素と表示要素のスクロールを連動させる
+  useEffect(() => {
+    const textMoaiElement = textMoaiRef.current;
+    if (textMoaiElement) {
+      const handleScroll = () => {
+        const scrollTop = textMoaiRef.current?.scrollTop;
+        const displayAreaElement = displayAreaRef.current;
+        if (displayAreaElement) {
+          displayAreaElement.scrollTo({ top: scrollTop });
+        }
+      };
+      textMoaiElement.addEventListener("scroll", handleScroll);
+      return () => {
+        textMoaiElement.removeEventListener("scroll", handleScroll);
+      };
+    }
+  }, []);
+
   return (
     <div className={styles.bodyWrapper}>
-      <form id="translateToMoai" onSubmit={handleSubmit(translateToMoai)}>
+      <form
+        id="translateToMoai"
+        onSubmit={handleSubmit(translateToMoai)}
+        className={styles.formElement}
+      >
         <textarea
           className={styles.textarea}
-          // type="text"
           id="textJp"
           {...register("textJp")}
           placeholder="こんにちは"
         />
+        <span className={styles.error}>{formState.errors.textJp?.message}</span>
       </form>
       <div className={styles.buttonWrapper}>
         <button type="submit" form="translateToMoai" className={styles.button}>
@@ -32,14 +103,36 @@ const Presenter: React.FC<Props> = ({ translateToMoai, translateToJp }) => {
           日本語に変換
         </button>
       </div>
-      <form id="translateToJp" onSubmit={handleSubmit(translateToJp)}>
+      <form
+        id="translateToJp"
+        onSubmit={handleSubmit(translateToJp, translateInvalidToJp)}
+        className={clsx(styles.formElement, styles.moaiTextWrapper)}
+      >
+        <div className={styles.displayArea} ref={displayAreaRef}>
+          {styledMoaiLangText}
+        </div>
         <textarea
           className={styles.textarea}
-          // type="text"
           id="textMoai"
           {...register("textMoai")}
-          placeholder="モーアモーiモァィモァイモアー"
+          ref={textMoaiRef}
+          onInput={(e) => {
+            setValue("textMoai", e.currentTarget.value);
+            handleChangeMoaiLang(e.currentTarget.value);
+          }}
+          placeholder="モーアモーｨモァィモァイモアー"
         />
+        <span className={styles.validationError}>
+          {formState.errors.textMoai?.message?.split("*").map((text, index) => {
+            return index % 2 === 0 ? (
+              <React.Fragment key={index}>{text}</React.Fragment>
+            ) : (
+              <span key={index} className={styles.error}>
+                {text}
+              </span>
+            );
+          })}
+        </span>
       </form>
     </div>
   );
--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,39 +1,110 @@
-import { FormProvider, SubmitHandler, useForm } from "react-hook-form";
+import {
+  FormProvider,
+  SubmitErrorHandler,
+  SubmitHandler,
+  useForm,
+} from "react-hook-form";
 import { MOAI_GO } from "../../tools/moaiLanguage";
 import Presenter from "./Presenter";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as schemas from "../../validations/schemas";
+import { useState } from "react";
 
 export type FormType = {
-  textJp: string;
-  textMoai: string;
+  textJp?: string;
+  textMoai?: string;
 };
+
+const schema = yup.object({
+  textJp: yup.string(),
+  textMoai: schemas.moaiLang,
+});
 
 const Home = () => {
   const methods = useForm<FormType>({
+    mode: "onChange",
+    resolver: yupResolver(schema),
     defaultValues: {
       textJp: "",
       textMoai: "",
     },
   });
 
-  const { setValue } = methods;
+  const { setValue, getValues } = methods;
 
+  // 日本語をモアイ語に変換する
   const translateToMoai: SubmitHandler<FormType> = (data) => {
+    if (!data.textJp) return;
+
     const textMoai = data.textJp
       .split("")
       .map((char) => MOAI_GO[char])
       .join("");
-    setValue("textMoai", textMoai);
+    const textMoaiElement = document.getElementById(
+      "textMoai"
+    ) as HTMLTextAreaElement;
+    if (textMoaiElement) textMoaiElement.value = textMoai;
+    handleChangeMoaiLang(textMoai);
   };
 
+  // バリデーションエラーがある時の処理
+  const translateInvalidToJp: SubmitErrorHandler<FormType> = () => {
+    const value = getValues("textMoai");
+    translateToJp({ textMoai: value });
+  };
+
+  // モアイ語を日本語に変換する
   const translateToJp: SubmitHandler<FormType> = (data) => {
+    if (!data.textMoai) return;
+
     const pattern = Object.values(MOAI_GO).reverse().join("|");
     const regexp = new RegExp(pattern, "g");
-    const textJp = data.textMoai.replace(regexp, (matched) => {
-      const a = Object.values(MOAI_GO).find((value) => value === matched);
-      return a ? a[0] : matched;
+    const textJp = data.textMoai.replace(regexp, (char) => {
+      const matchedEntries = Object.entries(MOAI_GO).find(
+        ([key, value]) => value === char
+      );
+      return matchedEntries ? matchedEntries[0] : char;
     });
 
     setValue("textJp", textJp);
+  };
+
+  const [notMoaiLangIndices, setNotMoaiLangIndices] = useState<number[]>([]);
+  const [moaiLangText, setMoaiLangText] = useState<string>("");
+
+  const handleChangeMoaiLang = (value: string) => {
+    setMoaiLangText(value);
+
+    const PATTERN = Object.values(MOAI_GO).reverse().join("|");
+    const allMoaiLangRegExp = new RegExp(`^(${PATTERN}|\\s)*$`);
+    const moaiLangRegExp = new RegExp(`${PATTERN}|\\s`, "g");
+    let match;
+
+    // マッチしない部分のインデックスを保存する配列
+    const errorIndices: number[] = [];
+
+    // 全体が正しいかどうかチェック
+    if (!allMoaiLangRegExp.test(value)) {
+      // 許可されているパターンをすべてサーチし、その範囲を除外
+      let lastIndex = 0;
+      while ((match = moaiLangRegExp.exec(value)) !== null) {
+        if (match.index > lastIndex) {
+          // マッチしない部分のインデックスを記録
+          for (let i = lastIndex; i < match.index; i++) {
+            errorIndices.push(i);
+          }
+        }
+        lastIndex = match.index + match[0].length;
+      }
+      // 末尾(後半)のマッチしない部分をチェック
+      if (lastIndex < value.length) {
+        for (let i = lastIndex; i < value.length; i++) {
+          errorIndices.push(i);
+        }
+      }
+    }
+    setNotMoaiLangIndices(errorIndices);
   };
 
   return (
@@ -41,6 +112,10 @@ const Home = () => {
       <Presenter
         translateToMoai={translateToMoai}
         translateToJp={translateToJp}
+        translateInvalidToJp={translateInvalidToJp}
+        moaiLangText={moaiLangText}
+        notMoaiLangIndices={notMoaiLangIndices}
+        handleChangeMoaiLang={handleChangeMoaiLang}
       />
     </FormProvider>
   );
