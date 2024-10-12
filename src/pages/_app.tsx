e91481ed6dfa1c26120616620915fe5236f3aef5
Hiroto-Shida <uecshida6295@gmail.com>
2024-10-13 01:10:52 +0900

    全体cssの調整

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { M_PLUS_Rounded_1c } from "next/font/google";
+
+const mPlusRounded1c = M_PLUS_Rounded_1c({
+  subsets: ["latin"],
+  weight: ["400"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={mPlusRounded1c.className}>
+      <Component {...pageProps} />;
+    </main>
+  );
 }
