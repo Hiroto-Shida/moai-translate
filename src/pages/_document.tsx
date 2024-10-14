4cae1a3c3d7a1739255450c49c9627ddbc69dff5
Hiroto-Shida <uecshida6295@gmail.com>
2024-10-14 13:24:46 +0900

    favicon仮設定

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="ja">
-      <Head />
+      <Head>
+        {/* TODO: 翻訳モアイのfavicon設定 */}
+        <link rel="icon" href="/moai_normal.png" />
+      </Head>
       <body>
         <Main />
         <NextScript />
