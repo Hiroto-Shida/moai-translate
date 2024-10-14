641456024dcfc171d446ed905076540b2ee1cff2
Hiroto-Shida <uecshida6295@gmail.com>
2024-10-14 18:09:35 +0900

    imgタグを使用したogp設定に変更

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
 import { M_PLUS_Rounded_1c } from "next/font/google";
+import Head from "next/head";
 
 const mPlusRounded1c = M_PLUS_Rounded_1c({
   subsets: ["latin"],
@@ -9,8 +10,13 @@ const mPlusRounded1c = M_PLUS_Rounded_1c({
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className={mPlusRounded1c.className}>
-      <Component {...pageProps} />
-    </main>
+    <>
+      <Head>
+        <title>モアイ翻訳</title>
+      </Head>
+      <main className={mPlusRounded1c.className}>
+        <Component {...pageProps} />
+      </main>
+    </>
   );
 }
--- src/pages/api/ogp.tsx
+++ src/pages/api/ogp.tsx
@@ -4,35 +4,53 @@ export const config = {
   runtime: "edge",
 };
 
+// const loadLocalImage = async (path: string) => {
+//   const fs = require("fs").promises;
+//   const data = await fs.readFile(path);
+//   return data;
+// };
+
 export default async function handler() {
   // NOTE: テキストをパラメータで指定することで動的な文章の表示が行える
   // const { searchParams } = new URL(req.url);
   // const text = searchParams.get("text");
+  // const imagePath = await loadLocalImage(`${process.env.NEXT_PUBLIC_VERCEL_URL}/ogp_background.jpg`);
+
+  const backgroundUrl = `${process.env.NEXT_PUBLIC_BASE_URL}/ogp_background.jpg`;
 
   return new ImageResponse(
     (
       <div
         style={{
-          backgroundImage: `url(${process.env.NEXT_PUBLIC_VERCEL_URL}/ogp_background.jpg)`,
-          backgroundSize: "100% 100%",
-          height: "100%",
-          width: "100%",
+          width: "1200px",
+          height: "630px",
           display: "flex",
-          padding: "0 120px",
-          flexDirection: "column",
+          alignItems: "center",
           justifyContent: "center",
+          position: "relative",
         }}
       >
-        <div
+        {/* 背景画像 */}
+        <img
+          src={backgroundUrl}
+          alt="Background"
           style={{
-            fontSize: 40,
-            color: "black",
+            position: "absolute",
+            top: 0,
+            left: 0,
             width: "100%",
             height: "100%",
-            padding: "50px 200px",
-            textAlign: "center",
-            justifyContent: "center",
-            alignItems: "center",
+            objectFit: "cover",
+            zIndex: -1,
+          }}
+        />
+        {/* テキスト */}
+        <div
+          style={{
+            color: "white",
+            fontSize: "48px",
+            fontWeight: "bold",
+            textShadow: "2px 2px 4px rgba(0, 0, 0, 0.5)",
           }}
         >
           モアイ翻訳
