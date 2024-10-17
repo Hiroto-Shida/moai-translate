c72b937cf201a0a33c26ff18ab98b2d052221e67
Hiroto-Shida <uecshida6295@gmail.com>
2024-10-17 10:04:33 +0900

    metaにcacheしない様な設定を追加

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,6 +9,12 @@ export default function Document() {
           property="og:image"
           content="https://moai-translate.vercel.app/api/ogp"
         />
+        <meta
+          http-equiv="Cache-Control"
+          content="no-cache, no-store, must-revalidate"
+        />
+        <meta http-equiv="Pragma" content="no-cache" />
+        <meta http-equiv="Expires" content="0" />
       </Head>
       <body>
         <Main />
