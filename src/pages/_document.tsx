d3fe193858f57f60c6c0bd2d1a4d157de688cee3
Hiroto-Shida <uecshida6295@gmail.com>
2024-10-17 10:11:45 +0900

    metaタグのcontent読み込みにクエリを追加

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,15 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
+  const timestamp = new Date().getTime(); // 現在の時間をミリ秒単位で取得
+
   return (
     <Html lang="ja">
       <Head>
         <link rel="icon" href="/moai_normal.png" />
         <meta
           property="og:image"
-          content="https://moai-translate.vercel.app/api/ogp"
+          content={`https://moai-translate.vercel.app/api/ogp?${timestamp}`}
         />
         <meta
           http-equiv="Cache-Control"
